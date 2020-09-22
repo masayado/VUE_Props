3df002d25506614a9b7e8760eb22990cd7292945
masayado <masayado@uc.cl>
2020-09-22 16:45:47 -0300

    inicie y configure proyecto en VUE JS

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: 'To-Do List',
+      };
+    }
+  }
+</script>
+
+<style scoped>
+  #app {
+    font-size: 25px;
+    font-family: "Calibri", sans-serif;
+  }
+</style>
